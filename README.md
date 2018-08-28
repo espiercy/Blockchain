db1d91d051522015969cf7977ca6b41dce1448d1
Evan <evanpiercy@gmail.com>
2018-08-27 19:03:13 -0700

    simple blockchain cycle

---
+++ README.md
@@ -0,0 +1,2 @@
+# Blockchain
+Startup blockchain application
--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,5 @@
 blockchain = []
+transaction_count = 0
 
 def get_last_blockchain_value():
     return blockchain[-1]
@@ -7,8 +8,16 @@ def get_last_blockchain_value():
 def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
-add_value(4.3)
-add_value(2.9, get_last_blockchain_value())
-add_value(0.8, get_last_blockchain_value())
 
-print(blockchain)
+def ask_input():
+    global transaction_count
+    transaction_count +=1
+    tx_amount = float(input('Your transaction amount for ' + str(transaction_count) + ': '))
+    add_value(tx_amount)
+    print(blockchain)
+
+
+tx_total_count = int(input("Enter the number of transactions you wish to complete: "))
+
+for i in range(tx_total_count):
+    ask_input()

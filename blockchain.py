1881aefe81f449bd16e57517f82295c86d1fa349
Evan <evanpiercy@gmail.com>
2018-08-29 05:43:07 -0700

    first transaction update

--- blockchain.py
+++ blockchain.py
@@ -5,6 +5,8 @@ transaction_count = 0
 #functions
 def get_last_blockchain_value():
     """ Returns the last value of current blockchain"""
+    if len(blockchain) < 1:
+       return None
     return blockchain[-1]
 
 
@@ -16,7 +18,10 @@ def add_value(transaction_amount, last_transaction=[1]):
        last_transaction: The previous blockchain transaction (default [1]) 
     
     """
-    blockchain.append([last_transaction, transaction_amount])
+    if last_transaction == None:
+        last_transaction = [1]
+    else:
+        blockchain.append([last_transaction, transaction_amount])
 
 
 def get_user_input():
@@ -24,11 +29,34 @@ def get_user_input():
     global transaction_count
     transaction_count +=1
     tx_amount = float(input('Your transaction amount for ' + str(transaction_count) + ': '))
-    add_value(tx_amount)
-    print(blockchain)
+    if blockchain != []:
+        add_value(tx_amount, get_last_blockchain_value())
+    else:
+        add_value(tx_amount)
 
 
-tx_total_count = int(input("Enter the number of transactions you wish to complete: "))
+def get_user_choice():
+    print('Please choose an option: ')
+    return input('1: Add new transaction value\n2: Print blockchain\nq: Quit\nChoice: ')
 
-for i in range(tx_total_count):
-    get_user_input()
+
+def print_blockchain():
+    for block in blockchain:
+        print("Outputting Block: ")
+        print(block)
+
+
+while(True):
+    choice = get_user_choice()
+    if choice == '1':
+        get_user_input()
+    elif choice == '2':
+        print_blockchain()
+    elif choice == 'q':
+        break
+    else:
+        print('Invalid choice! Choose again.')
+    print()
+
+
+print('Goodbye!')

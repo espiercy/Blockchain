0559ac63068ce9a04785d91ce22deffcea05e59b
Evan <evanpiercy@gmail.com>
2018-09-15 04:34:02 -0700

    file storage

--- blockchain.py
+++ blockchain.py
@@ -1,6 +1,6 @@
 from functools import reduce
 from collections import OrderedDict
-import hashlib as hl
+import hashlib as hl, json, pickle
 
 from hash_util import hash_string_256, hash_block
 
@@ -21,6 +21,49 @@ transaction_count = 0
 participants = {'Evan'}
 
 #functions
+def load_data():
+    with open('blockchain.txt', mode='r') as f:
+        #file_content = pickle.loads(f.read())
+        file_content = f.readlines()
+        global blockchain
+        global open_transactions
+        # blockchain = file_content['chain']
+        # open_transactions = file_content['ot']
+        blockchain = json.loads(file_content[0][:-1])
+        updated_blockchain = []
+        for block in blockchain:
+            updated_block = {
+               'previous_hash': block['previous_hash'],
+               'index': block['index'],
+               'proof': block['proof'],
+               'transactions': [OrderedDict(
+                   [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])]) for tx in block['transactions']]
+           }
+            updated_blockchain.append(updated_block)
+        blockchain = updated_blockchain
+        open_transactions = json.loads(file_content[1])
+        updated_transactions = []
+        for tx in open_transactions:
+            updated_transaction = OrderedDict(
+                [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])])
+            updated_transactions.append(updated_transaction)
+        open_transactions = updated_transactions
+
+
+load_data()
+
+def save_data():
+    with open('blockchain.txt', mode='w') as f:
+        f.write(json.dumps(blockchain))
+        f.write('\n')
+        f.write(json.dumps(open_transactions))
+        # save_data = {
+        #     'chain': blockchain,
+        #     'ot': open_transactions
+        # }
+        # f.write(pickle.dumps(save_data))
+
+
 def valid_proof(transactions, last_hash, proof):
     guess = (str(transactions) + str(last_hash) + str(proof)).encode()
     guess_hash = hash_string_256(guess)
@@ -74,6 +117,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -159,6 +203,7 @@ while waiting_for_input:
     elif choice == '2':
         if mine_block():
             open_transactions = []
+            save_data()
     elif choice == '3':
         print_blockchain()
     elif choice == '4':
